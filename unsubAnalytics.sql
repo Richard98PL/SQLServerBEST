dae52a2a2e0614636eb9c41d620e1b2c1cb893e8
Richard98PL <41301282+Richard98PL@users.noreply.github.com>
2022-11-24 18:16:31 +0100

    Update unsubAnalytics.sql

--- unsubAnalytics.sql
+++ unsubAnalytics.sql
@@ -5,29 +5,56 @@ OptOutType,
 OptOutQuantity, 
 CASE 
     WHEN JourneySends != 0 THEN JourneySends
-    WHEN JourneySends = 0 THEN ''
+    WHEN JourneySends = 0 THEN null
     END AS 'JourneySends',
 CONCAT(
     CONVERT(
         varchar(500), 
         ROUND(
-                CASE WHEN JourneySends = 0 THEN 0
+                CASE WHEN JourneySends = 0 THEN null
                 ELSE CAST(OptOutQuantity*100 as float)/CAST(JourneySends as float)
                 END,
                 2
             )
         ), 
-'%') as 'Ratio'
+CASE WHEN JourneySends = 0 THEN null ELSE '%' END) as 'OptOutToJourneySendRatio',
+
+CASE 
+    WHEN JourneyVersionSends != 0 THEN JourneyVersionSends
+    WHEN JourneyVersionSends = 0 THEN null
+    END AS 'JourneyVersionSends',
+CONCAT(
+    CONVERT(
+        varchar(500), 
+        ROUND(
+                CASE WHEN JourneyVersionSends = 0 THEN null
+                ELSE CAST(OptOutQuantity*100 as float)/CAST(JourneyVersionSends as float)
+                END,
+                2
+            )
+        ), 
+CASE WHEN JourneyVersionSends = 0 THEN null ELSE '%' END) as 'OptOutToJourneyVersionSendRatio'
+
 FROM (
     SELECT 
     JourneyName, CONCAT(JourneyName, ' - ', VersionNumber) AS 'VersionNumber', [Type], OptOutType, 
     Count(Id) AS 'OptOutQuantity',
+    
     (SELECT COUNT(*) FROM _Sent
     INNER JOIN _JourneyActivity ja 
     ON TriggererSendDefinitionObjectId = ja.JourneyActivityObjectId
     INNER JOIN _Journey j
     ON j.VersionId = ja.VersionId
-    WHERE j.JourneyName = u.JourneyName) AS 'JourneySends'
+    WHERE j.JourneyName = u.JourneyName) AS 'JourneySends',
+    
+    (SELECT COUNT(*) FROM _Sent
+    INNER JOIN _JourneyActivity ja 
+    ON TriggererSendDefinitionObjectId = ja.JourneyActivityObjectId
+    INNER JOIN _Journey j
+    ON j.VersionId = ja.VersionId
+    WHERE CONCAT(j.JourneyName, ' - ', j.VersionNumber) =
+          CONCAT(u.JourneyName, ' - ', u.VersionNumber)) AS 'JourneyVersionSends'
+    
     FROM Unsubscribes u
     WHERE JourneyName <> '' AND VersionId <> '' AND [Type] <> ''
     GROUP BY ROLLUP (
