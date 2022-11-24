fdecd597bd820f223123446a6e298c5e710475f8
Richard98PL <41301282+Richard98PL@users.noreply.github.com>
2022-11-24 21:22:32 +0100

    Update unsubAnalytics.sql

--- unsubAnalytics.sql
+++ unsubAnalytics.sql
@@ -33,7 +33,34 @@ CONCAT(
                 2
             )
         ), 
-CASE WHEN JourneyVersionSends = 0 THEN null ELSE '%' END) as 'OptOutToJourneyVersionSendRatio'
+CASE WHEN JourneyVersionSends = 0 THEN null ELSE '%' END) as 'OptOutToJourneyVersionSendRatio',
+
+
+
+
+
+CASE 
+    WHEN AllVersionReceiversOfType != 0 THEN AllVersionReceiversOfType
+    WHEN AllVersionReceiversOfType = 0 THEN null
+    END AS 'AllVersionReceiversOfType',
+CONCAT(
+    CONVERT(
+        varchar(500), 
+        ROUND(
+                CASE WHEN AllVersionReceiversOfType = 0 THEN null
+                ELSE CAST(OptOutQuantity*100 as float)/CAST(AllVersionReceiversOfType as float)
+                END,
+                2
+            )
+        ), 
+CASE WHEN AllVersionReceiversOfType = 0 THEN null ELSE '%' END) as 'OptOutToJourneyVersionSendPerTypeRatio'
+
+
+
+
+
+
+
 
 FROM (
     SELECT 
@@ -53,7 +80,21 @@ FROM (
     INNER JOIN _Journey j
     ON j.VersionId = ja.VersionId
     WHERE CONCAT(j.JourneyName, ' - ', j.VersionNumber) =
-          CONCAT(u.JourneyName, ' - ', u.VersionNumber)) AS 'JourneyVersionSends'
+          CONCAT(u.JourneyName, ' - ', u.VersionNumber)) AS 'JourneyVersionSends',
+    
+    (SELECT COUNT(Distinct SubscriberId) FROM _Sent s
+        INNER JOIN _JourneyActivity ja 
+        ON TriggererSendDefinitionObjectId = ja.JourneyActivityObjectId
+        INNER JOIN _Journey j
+        ON j.VersionId = ja.VersionId
+        WHERE CONCAT(j.JourneyName, ' - ', j.VersionNumber) =
+              CONCAT(u.JourneyName, ' - ', u.VersionNumber)
+       AND s.SubscriberKey LIKE
+       CASE
+        WHEN u.[Type] = 'WebCollect' THEN '%@%'
+        WHEN u.[Type] = 'Lead' THEN '%00Q%'
+        WHEN u.[Type] = 'Contact' THEN '%003%'
+     END) AS 'AllVersionReceiversOfType'
     
     FROM Unsubscribes u
     WHERE JourneyName <> '' AND VersionId <> '' AND [Type] <> ''
