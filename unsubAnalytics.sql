8d6a9426d38d66b5976970b3c10d682f93a3b82b
Richard98PL <41301282+Richard98PL@users.noreply.github.com>
2022-11-25 10:56:23 +0100

    Update unsubAnalytics.sql

--- unsubAnalytics.sql
+++ unsubAnalytics.sql
@@ -1,59 +1,62 @@
-SELECT JourneyName, 
-VersionNumber, 
-[Type], 
-OptOutType, 
-OptOutQuantity, 
+SELECT 
+[Journey Name], 
+[Version Number], 
+[Customer Group],
+CONVERT(varchar(500), [Opt-Out Type]) as 'Opt-Out Type',
+[Selected - Opt-Outs], 
+
 CASE 
-    WHEN JourneySends != 0 THEN JourneySends
-    WHEN JourneySends = 0 THEN null
-    END AS 'JourneySends',
+    WHEN [Journey - All Sends] != 0 THEN [Journey - All Sends]
+    WHEN [Journey - All Sends] = 0 THEN null
+    END AS 'Journey - All Sends',
 CONCAT(
     CONVERT(
         varchar(500), 
         ROUND(
-                CASE WHEN JourneySends = 0 THEN null
-                ELSE CAST(OptOutQuantity*100 as float)/CAST(JourneySends as float)
+                CASE WHEN [Journey - All Sends] = 0 THEN null
+                ELSE CAST([Selected - Opt-Outs]*100 AS float)/CAST([Journey - All Sends] AS float)
                 END,
                 2
             )
         ), 
-CASE WHEN JourneySends = 0 THEN null ELSE '%' END) as 'OptOutToJourneySendRatio',
+CASE WHEN [Journey - All Sends] = 0 THEN null ELSE '%' END) AS 'Journey - Opt-Out Ratio',
+
 
 CASE 
-    WHEN JourneyVersionSends != 0 THEN JourneyVersionSends
-    WHEN JourneyVersionSends = 0 THEN null
-    END AS 'JourneyVersionSends',
+    WHEN [Version - All Sends] != 0 THEN [Version - All Sends]
+    WHEN [Version - All Sends] = 0 THEN null
+    END AS 'Version - All Sends',
 CONCAT(
     CONVERT(
         varchar(500), 
         ROUND(
-                CASE WHEN JourneyVersionSends = 0 THEN null
-                ELSE CAST(OptOutQuantity*100 as float)/CAST(JourneyVersionSends as float)
+                CASE WHEN [Version - All Sends] = 0 THEN null
+                ELSE CAST([Selected - Opt-Outs]*100 AS float)/CAST([Version - All Sends] AS float)
                 END,
                 2
             )
         ), 
-CASE WHEN JourneyVersionSends = 0 THEN null ELSE '%' END) as 'OptOutToJourneyVersionSendRatio',
+CASE WHEN [Version - All Sends] = 0 THEN null ELSE '%' END) AS 'Version - Opt-Out Ratio',
 
 
 
 
 
 CASE 
-    WHEN AllVersionReceiversOfType != 0 THEN AllVersionReceiversOfType
-    WHEN AllVersionReceiversOfType = 0 THEN null
-    END AS 'AllVersionReceiversOfType',
+    WHEN [Version - All Sends To Customer Group] != 0 THEN [Version - All Sends To Customer Group]
+    WHEN [Version - All Sends To Customer Group] = 0 THEN null
+    END AS 'Version - All Sends To Customer Group',
 CONCAT(
     CONVERT(
         varchar(500), 
         ROUND(
-                CASE WHEN AllVersionReceiversOfType = 0 THEN null
-                ELSE CAST(OptOutQuantity*100 as float)/CAST(AllVersionReceiversOfType as float)
+                CASE WHEN [Version - All Sends To Customer Group] = 0 THEN null
+                ELSE CAST([Selected - Opt-Outs]*100 AS float)/CAST([Version - All Sends To Customer Group] AS float)
                 END,
                 2
             )
         ), 
-CASE WHEN AllVersionReceiversOfType = 0 THEN null ELSE '%' END) as 'OptOutToJourneyVersionSendPerTypeRatio'
+CASE WHEN [Version - All Sends To Customer Group] = 0 THEN null ELSE '%' END) AS 'Version - Opt-Out Ratio in Selected Group'
 
 
 
@@ -64,15 +67,18 @@ CASE WHEN AllVersionReceiversOfType = 0 THEN null ELSE '%' END) as 'OptOutToJour
 
 FROM (
     SELECT 
-    JourneyName, CONCAT(JourneyName, ' - ', VersionNumber) AS 'VersionNumber', [Type], OptOutType, 
-    Count(Id) AS 'OptOutQuantity',
+    JourneyName AS 'Journey Name', 
+    CONCAT(JourneyName, ' - ', VersionNumber) AS 'Version Number',
+    [Type] AS 'Customer Group',
+    CONVERT(varchar(500), OptOutType) as 'Opt-Out Type',
+    Count(Id) AS 'Selected - Opt-Outs',
     
     (SELECT COUNT(*) FROM _Sent
     INNER JOIN _JourneyActivity ja 
     ON TriggererSendDefinitionObjectId = ja.JourneyActivityObjectId
     INNER JOIN _Journey j
     ON j.VersionId = ja.VersionId
-    WHERE j.JourneyName = u.JourneyName) AS 'JourneySends',
+    WHERE j.JourneyName = u.JourneyName) AS 'Journey - All Sends',
     
     (SELECT COUNT(*) FROM _Sent
     INNER JOIN _JourneyActivity ja 
@@ -80,7 +86,7 @@ FROM (
     INNER JOIN _Journey j
     ON j.VersionId = ja.VersionId
     WHERE CONCAT(j.JourneyName, ' - ', j.VersionNumber) =
-          CONCAT(u.JourneyName, ' - ', u.VersionNumber)) AS 'JourneyVersionSends',
+          CONCAT(u.JourneyName, ' - ', u.VersionNumber)) AS 'Version - All Sends',
     
     (SELECT COUNT(Distinct SubscriberId) FROM _Sent s
         INNER JOIN _JourneyActivity ja 
@@ -91,15 +97,15 @@ FROM (
               CONCAT(u.JourneyName, ' - ', u.VersionNumber)
        AND s.SubscriberKey LIKE
        CASE
-        WHEN u.[Type] = 'WebCollect' THEN '%@%'
-        WHEN u.[Type] = 'Lead' THEN '%00Q%'
-        WHEN u.[Type] = 'Contact' THEN '%003%'
-     END) AS 'AllVersionReceiversOfType'
+        WHEN u.[Type] = 'Newsletter Form' THEN '%@%'
+        WHEN u.[Type] = 'Cold Lead Dropout' THEN '%00Q%'
+        WHEN u.[Type] = 'VIP Customer' THEN '%003%'
+     END) AS 'Version - All Sends To Customer Group'
     
     FROM Unsubscribes u
     WHERE JourneyName <> '' AND VersionId <> '' AND [Type] <> ''
     GROUP BY ROLLUP (
     JourneyName, CONCAT(JourneyName, ' - ', VersionNumber), [Type], OptOutType
     ) 
-) as X
-WHERE JourneyName <> ''
+) AS X
+WHERE [Journey Name] <> ''
